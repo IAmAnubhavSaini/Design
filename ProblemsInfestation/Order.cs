0b803d925e914455c91203330ed20f996023e112
Anubhav Saini <iamanubhavsaini@outlook.com>
2014-07-29 10:40:10 +0530

    Renaming. ZOI.

---
+++ ProblemsInfestation/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemsInfestation
+{
+    public class Order
+    {
+        public Guid OrderId { get; set; }
+        public Customer Customer { get; set; }
+        // Following arbitrary limit of 64 on products an order can contain is
+        // violating Zero-One-Infinity rule
+        public Product[] Products = new Product[64];
+    }
+}
---
+++ ProblemsSolution_ZOI/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemsSolution_ZOI
+{
+    
+    public class Order
+    {
+        public Guid OrderId { get; set; }
+        public Customer Customer { get; set; }
+
+        // How we kill the ZOI problem: refer ProblemsInfestation.Order
+        public List<Product> Products = new List<Product>(); 
+    }
+
+    
+    /* for sake of not getting errors while accessing Customer or Product*/
+
+    public class Customer
+    {
+        public Guid CustomerId { get; set; }
+        public string CustomerName { get; set; }
+    }
+
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
